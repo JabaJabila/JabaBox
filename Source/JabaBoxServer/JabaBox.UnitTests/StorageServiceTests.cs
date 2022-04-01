f9d149e30895107d2d4fde394d6dd5b3ebbd29ae
Andreev Artem <andreev_3731@mail.ru>
2022-04-01 17:48:46 +0300

    feat: add projects and dependencies

---
+++ Source/JabaBoxServer/JabaBox.UnitTests/StorageServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace JabaBox.UnitTests;
+
+[TestFixture]
+public class StorageServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
